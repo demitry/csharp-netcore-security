a3e9cb8c6f32c37a6a59750b0ba591ad6a7c5965
demitry <dpoluektov@gmail.com>
2023-10-09 12:28:18 +0300

    Xss Test Sample

---
+++ MySamples/XssTest/XssTest/Pages/SearchPageModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XssTest.Pages;
+
+public class SearchPageModel : PageModel
+{
+    public string Result { get; set; } = string.Empty;
+    public void OnGet(string searchTerm)
+    {
+        Result = string.IsNullOrEmpty(searchTerm) ?
+            string.Empty :
+            $"Your search for <i>{searchTerm}</i> did not yield any results.";
+    }
+}
