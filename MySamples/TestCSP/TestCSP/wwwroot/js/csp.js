45466bf1788a493eaa10ced892ba8f6bebb879c1
demitry <dpoluektov@gmail.com>
2023-10-11 12:40:28 +0300

    Test CSP: 1) Underscore templating 2) External JS file 3) External Logo - they all works

---
+++ MySamples/TestCSP/TestCSP/wwwroot/js/csp.js
@@ -0,0 +1,5 @@
+﻿var $timeSpan = document.getElementById("timeSpan");
+var $tmpl = _.template("Current time: <%= time %>");
+function updateTime(timeString) {
+    $timeSpan.innerHTML = $tmpl({ time: timeString });
+}
