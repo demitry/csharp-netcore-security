566bb1bc1e3a0ea6ba36cfc42b0a3200b063ec1c
demitry <dpoluektov@gmail.com>
2023-10-11 20:11:45 +0300

    Broke and fix with CSP

--- MySamples/TestCSP/TestCSP/Models/CSPModel.cs
+++ MySamples/TestCSP/TestCSP/Models/CSPModel.cs
@@ -6,7 +6,8 @@ namespace TestCSP.Models
     {
         public void OnGet()
         {
-            var cspHeader = "default-src 'self'";
+            var cspHeader =
+                "default-src 'self'; img-src 'self' https://www.manning.com/assets/; style-src 'self' https://cdn.jsdelivr.net 'unsafe-hashes' 'sha256-yckz1zrIL2HgQwm7x1ins99s5jndZE3XnmgOAkJvDOg='; script-src 'self' https://cdn.jsdelivr.net 'sha256-Nri3mDGBJy3LCT+AxvP4dtbCPf6tkg4QIcqNisVrRZw=' 'unsafe-eval'";
             HttpContext.Response.Headers.Add("Content-Security-Policy", cspHeader);
         }
     }
