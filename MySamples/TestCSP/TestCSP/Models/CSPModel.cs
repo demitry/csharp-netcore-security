cb3b488c1268f5f839b8b6ac7ad93cc8012583b6
demitry <dpoluektov@gmail.com>
2023-10-11 12:58:18 +0300

    With the CSP policy broke anything: templating, JS clock, external logo

---
+++ MySamples/TestCSP/TestCSP/Models/CSPModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestCSP.Models
+{
+    public class CspModel : PageModel
+    {
+        public void OnGet()
+        {
+            var cspHeader = "default-src 'self'";
+            HttpContext.Response.Headers.Add("Content-Security-Policy", cspHeader);
+        }
+    }
+}
