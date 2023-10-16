2573b5daf7f0a770058e7a62e6866f72f66140e0
demitry <dpoluektov@gmail.com>
2023-10-16 23:31:40 +0300

    Remove Bad sample

--- MySamples/TestCrossSiteRf/TestCrossSiteRf/Models/ShoppingCartItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TestCrossSiteRf.Models;
-
-public class ShoppingCartItem
-{
-    public string Name { get; set; } = string.Empty;
-    public int Quantity { get; set; }
-    public double Price { get; set; }
-}
